863a96e21349652168e7c8298c2f3203b227dc8f
Jeff Baker <git@jbaker.net>
2023-05-27 10:29:55 -0600

    Creating Star Wars JS file

---
+++ starwars/starwars.js
@@ -0,0 +1,30 @@
+const seriesName = "the Marvel Cinematic Universe";
+const pageTitle = `Best Order to Watch ${seriesName}`;
+const pageName = "Marvel Cinematic Universe";
+const pageNameMobile = "Marvel";
+
+// How to describe the people who consume the media in question (e.g. "viewers," "readers," "gamers")
+const userType = "viewers";
+const progressiveVerb = "watching";
+const infinitiveVerb = "watch";
+const greetingsName = "Superhero"
+
+// How to describe how someone might view the media in "tailer" or "blurb" form
+const blurbVerb = "watching a trailer";
+
+// How to describe the people who made the media
+const mediaCreators = "filmmakers";
+
+// Example of a subseries
+const subseriesExample = "Thor";
+
+// Type of image being used
+const imageType = "poster";
+
+const typeOptions = [
+    ["Films", "films"],
+    ["TV Series", "tvseries"],
+    ["Animation", "animation"],
+    ["Jr. Animation", "jranimation"],
+    ["Shorts", "shorts"]
+]
