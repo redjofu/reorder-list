8f04767c46bef3557f7b4d6b9d924a1a1b9f6100
Jeff Baker <git@jbaker.net>
2023-05-30 21:43:46 -0600

    Adjustments to marvel.js and starwars.js

--- starwars/starwars.js
+++ starwars/starwars.js
@@ -28,3 +28,69 @@ const typeOptions = [
     ["Jr. Animation", "jranimation"],
     ["Shorts", "shorts"]
 ]
+
+const orderOptions = {
+    release: true,
+    chronological: true,
+    narrative: true,
+    alphabetical: true
+};
+
+// Are different elements present?
+// const lengthExists = true;
+// const subseriesExist = true;
+// const phasesExist = true;
+const creditScenesExist = true;
+const otherSeriesMentions = true;
+
+// Names of sagas
+const sagaInfinity = "The Infinity Saga";
+const sagaMultiverse = "The Multiverse Saga";
+
+const sagaNames = [
+    "", // 0
+    sagaInfinity, sagaInfinity, sagaInfinity, // 1-3
+    sagaMultiverse, sagaMultiverse, sagaMultiverse, // 4-6
+    "" // 7
+]
+
+// Text that appears when a selection input is clicked
+const selectionOptionDescription = {
+    // Type inputs
+    films: `Theatrical films produced by Marvel Studios. Marvel films produced by other studios (e.g. Fox's "X-Men" series) are not included.`,
+    tvseries: `Disney+ series produced by Marvel Studios. Series produced by Marvel TV (e.g. "Agents of S.H.I.E.L.D.") are not included.`,
+    animation: `Animated series produced by Marvel Studios and which are expressly tied to the Marvel Cinematic Universe.`,
+    specials: `Marvel Studios Special Presentations, styled after traditional TV specials and often themed for a holiday.`,
+    shorts: `Short films produced by Marvel Studios.`,
+
+
+    // Order inputs
+    chronological: `The order according to when the core events for each entry occur within the overarching story. This is primarily taken from the order listed within Disney+, though sources using orders compiled by fans are also used.`
+
+    // Spoiler inputs
+};
+
+const orderRec = {
+    releaseRec: true,
+    releaseFeeling: "good",
+    chronologicalRec: true,
+    chronologicalFeeling: "good",
+    // narrativeRec: true,
+    // narrativeFeeling: "excellent"
+}
+
+
+// Series-specific details
+const officialSite = {
+    url: "https://www.starwars.com/",
+    icon: "marvel.png",
+    description: "Official Star Wars Website"
+}
+
+const wiki = {
+    url: "https://marvelcinematicuniverse.fandom.com/wiki/",
+    icon: "fandom.png",
+    description: "Marvel Cinimatic Universe Wiki"
+}
+
+pageLoads = true; // This is used by the check-update file trying to find this JS file, and it should always be true.
