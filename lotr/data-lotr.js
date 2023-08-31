98a721476ab9e571b1e2dfec3d44c3396ee0233e
Jeff Baker <git@jbaker.net>
2023-08-31 07:31:36 -0600

    Add lotr files

---
+++ lotr/data-lotr.js
@@ -0,0 +1,13 @@
+const characters = {
+    lukeskywalker:{name:"Luke Skywalker",fullname:"",alias:"",gender:"m",nationality:"Tatooine",species:"human",race:"white",profession:"Jedi",},
+}
+
+const subseries = {
+    dawnjedi:{name:`Dawn of the Jedi`,},
+}
+
+const entries = [
+    {code:`new-hope`,name:`Episode IV: A New Hope`,release:`1977-05-25`,chronological:26,narrative:1,special:1,type:`films`,classification:`Film`,logo:`new-hope.png`,image:`new-hope.jpg`,imagespoiler:`pst`,length:125,lengthtype:`minutes`,subseries:{ primary : [subseries.agerebellion.name, subseries.agerebellion.sub1, "pst"] },disneyplus:`movies/star-wars-a-new-hope-episode-iv/12fVeZxD2fWJ`,netflix:`60010932`,netflixdvd:true,primevideo:`B00VF06OBS`,vudu:`8845`,appletv:`movie/star-wars-a-new-hope/umc.cmc.2o65qvudvwq1l1rqjlbyfszwn`,googleplay:`movies/details/Star_Wars_A_New_Hope?id=yYNSSNJ0z_U`,redbox:`star-wars-a-new-hope`,disc:true,official:`films/star-wars-episode-iv-a-new-hope`,wikipedia:`Star_Wars_(film)`,wiki:`Star_Wars:_Episode_IV_A_New_Hope`,rottentomatoes:`m/star_wars_episode_iv_a_new_hope`,rottencritics:93,rottenaudience:96,metacritic:`movie/star-wars-episode-iv---a-new-hope`,metascore:90,mcuserscore:8.8,mparating:`PG`,imdb:`tt0076759`,wikiquote:`Star_Wars_(film)`,youtube:`vZ734NWnAHA`,spotify:`album/55gMu4AvAKCbCaGv3GIXgy`,commonsense:`movie-reviews/star-wars-episode-iv-a-new-hope`,commonsenseage:7,kim:`s/star_wars_special_edition_1997_.htm`,kimrating:`0.4.2`,clearplay:`953`,},
+]
+
+dataLoads = true; // This is used by the check-update file trying to find this JS file, and it should always be true.
