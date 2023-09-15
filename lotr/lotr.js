38f411bafd9304f122c4edd3221f32cb62b82329
Jeff Baker <git@jbaker.net>
2023-09-15 06:00:52 -0600

    Finishing tweaks for lotr

--- lotr/lotr.js
+++ lotr/lotr.js
@@ -7,7 +7,7 @@ const pageNameMobile = "LotR";
 const userType = "viewers";
 const progressiveVerb = "watching";
 const infinitiveVerb = "watch";
-const greetingsName = "Elf"
+const greetingsName = "Master Hobbit"
 
 // How to describe how someone might view the media in "tailer" or "blurb" form
 const blurbVerb = "watching a trailer";
@@ -50,7 +50,7 @@ const selectionOptionDescription = {
     // Order inputs
     chronological: `The order according to when the core events for each entry occur within the history of Middle-earth.`,
     recommendChronological: `The release order for the live-action screen adapations is different than the book publishing order. Because of this, watching <em>The Lord of the Rings</em> films before <em>The Hobbit</em> films is the preferred narrative order, though beginning with <em>The Hobbit</em> since it is chronologically earlier is also acceptable. However, watching <em>The Rings of Power</em> first will not be as meaningful even though it takes place first chronologically.`,
-    narrative: `An order I think offers good narrative structure for viewers (including those watching for the first time), primarily built around continuing related plot threads.`,
+    narrative: `An order I think offers good narrative structure for viewers (including those watching for the first time). The emphasis is not on chronological events but rather on presenting an order which ties new entries to ones which the viewer has already seen in exciting ways.`,
 
     // Spoiler inputs
 };
@@ -77,7 +77,5 @@ const wiki = {
     icon: "fandom.png",
     description: "One Wiki to Rule Them All"
 }
-
-const cloneWarsChronLink = `The official chronological order for <em>the Clone Wars</em> <a href="https://www.starwars.com/news/star-wars-the-clone-wars-chronological-episodeorder">can be found at StarWars.com</a>.`;
 
 pageLoads = true; // This is used by the check-update file trying to find this JS file, and it should always be true.
